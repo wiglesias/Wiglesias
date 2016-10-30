7014a7a8920a726013385b5dbd6527ffb08e6cb8
Wils Iglesias <wiglesias83@gmail.com>
2016-10-30 09:55:37 +0100

    add class ContactMessage Entity and ContactMessageRepository

---
+++ src/AppBundle/Repository/ContactMessageRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Repository;
+
+/**
+ * Class ContactMessageRepository
+ *
+ * @category Repository
+ * @package AppBundle\Repository
+ * @autor Wils Iglesias <wiglesias83@gmail.com>
+ */
+class ContactMessageRepository
+{
+
+}
