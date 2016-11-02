345c1067f3ba7b8f7d508b5c3016dcdd2a79ba89
Wils Iglesias <wiglesias83@gmail.com>
2016-11-02 12:25:43 +0100

    improve BlogController add method postDetailAction

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
+use Doctrine\ORM\EntityNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,5 +18,36 @@ class BlogController extends Controller
     public function indexAction()
     {
         return $this->render(':Frontend/Blog:list.html.twig');
+    }
+
+    /**
+     * @Route("/blog/{year}/{month}/{day}/{slug}", name="front_blog_detail")
+     * @param $year
+     * @param $month
+     * @param $day
+     * @param $slug
+     *
+     * @return Response
+     * @throws EntityNotFoundException
+     */
+    public function postDetailAction($year, $month, $day, $slug)
+    {
+        $date = \DateTime::createFromFormat('Y-m-d', $year . '-' . $month . '-' . $day);
+
+        /** @var Post $post */
+        $post = $this->getDoctrine()->getRepository('AppBundle:Post')->findOneBy(
+            array(
+                'publishedAt' => $date,
+                'slug' => $slug,
+            )
+        );
+
+        if (!$post){
+            throw new EntityNotFoundException();
+        }
+
+        return $this->render(':Frontend/Blog:detail.html.twig',
+            [ 'post' => $post ]
+        );
     }
 }
