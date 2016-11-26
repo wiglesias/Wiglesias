c0548ada2cb60f99cccfb3437094574b0d4ccd96
Wils Iglesias <wiglesias83@gmail.com>
2016-11-26 14:15:11 +0100

    add pagination

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Post;
+use Doctrine\ORM\EntityNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,35 +11,49 @@ use Symfony\Component\HttpFoundation\Response;
 class BlogController extends Controller
 {
     /**
-     * @Route("/blog", name="front_blog")
+     * @Route("/blog/{pagina}", name="front_blog")
+     *
+     * @param int $pagina
      *
      * @return Response
+     *
      */
-    public function indexAction()
+    public function indexAction($pagina = 1)
     {
-        $posts = $this->getDoctrine()->getRepository('AppBundle:Post')->findAll();
+        $posts = $this->getDoctrine()->getRepository('AppBundle:Post')->getAllEnabledSortedByPublishedDateWithJoinUntilNow();
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($posts, $pagina, 9);
 
         return $this->render(':Frontend/Blog:list.html.twig', [
-            'posts' => $posts
+            'pagination' => $pagination,
         ]);
     }
 
     /**
-     * @Route("/blog/{slug}", name="front_blog_detail")
+     * @Route("/blog/{year}/{month}/{day}/{slug}", name="front_blog_detail")
+     * @param $year
+     * @param $month
+     * @param $day
      * @param $slug
      *
      * @return Response
+     * @throws EntityNotFoundException
      */
-    public function postDetailAction($slug)
+    public function postDetailAction($year, $month, $day, $slug)
     {
-        /**
-         * @var Post $post
-         */
+        $date = \DateTime::createFromFormat('Y-m-d', $year . '-' . $month . '-' . $day);
+
+        /** @var Post $post */
         $post = $this->getDoctrine()->getRepository('AppBundle:Post')->findOneBy(
             array(
-                'slug' => $slug
+                'publishedAt' => $date,
+                'slug' => $slug,
             )
         );
+
+        if (!$post) {
+            throw new EntityNotFoundException();
+        }
 
         return $this->render('Frontend/Blog/detail.html.twig',
             [ 'post' => $post ]
