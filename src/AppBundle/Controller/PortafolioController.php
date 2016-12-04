c38ac07dbc3620a5375962106e0bfcebfb872024
Wils Iglesias <wiglesias83@gmail.com>
2016-12-04 02:53:32 +0100

    load show portafolio controller

--- src/AppBundle/Controller/PortafolioController.php
+++ src/AppBundle/Controller/PortafolioController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\ORM\EntityNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,31 +12,47 @@ class PortafolioController extends Controller
     /**
      * @Route("/portafolio", name="front_portafolio")
      *
-     * @return  Response
+     * @param int $pagina
+     *
+     * @return Response
      */
-    public function listAction()
+    public function listAction($pagina = 1)
     {
         $allPortafolios = $this->getDoctrine()->getRepository('AppBundle:Portafolio')->findAllEnabledSortedByDate();
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($allPortafolios, $pagina, 9);
 
         return $this->render(':Frontend/Portafolio:portafolio.html.twig', [
+            'pagination' => $pagination,
             'allPortafolios' => $allPortafolios,
         ]);
     }
 
     /**
-     * @Route("/portafolio/{slug}", name="front_portafolio_detail")
+     * @Route("/portafolio/{year}/{month}/{day}/{slug}", name="front_portafolio_detail")
      *
+     * @param $year
+     * @param $month
+     * @param $day
      * @param string $slug
      *
      * @return Response
+     * @throws EntityNotFoundException
      */
-    public function detailAction($slug)
+    public function detailAction($year, $month, $day, $slug)
     {
+        $date = \DateTime::createFromFormat('Y-m-d', $year . '-' . $month - '-' . $day);
+
         $portafolio = $this->getDoctrine()->getRepository('AppBundle:Portafolio')->findAll(
             array(
+                'publishedAt' => $date,
                 'slug' => $slug,
             )
         );
+
+        if (!$portafolio) {
+            throw new EntityNotFoundException();
+        }
 
         return $this->render(':Frontend/Portafolio:detail.html.twig', array(
             'portafolio' => $portafolio,
