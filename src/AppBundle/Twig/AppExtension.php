0a72c0f70d54712299f3a07d7cbccb81b67b6b24
Wils Iglesias <wiglesias83@gmail.com>
2017-11-06 15:50:47 +0100

    delete delete tag user of list User admin

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -78,9 +78,7 @@ class AppExtension extends \Twig_Extension
         if ($object instanceof User && count($object->getRoles()) > 0) {
             /** @var string $role */
             foreach ($object->getRoles() as $role) {
-                if ($role == UserRolesEnum::ROLE_USER) {
-                    $span .= '<span class="label label-info" style="margin-right:10px">usuario</span>';
-                } else if ($role == UserRolesEnum::ROLE_CMS) {
+                if ($role == UserRolesEnum::ROLE_CMS) {
                     $span .= '<span class="label label-warning" style="margin-right:10px">editor</span>';
                 } else if ($role == UserRolesEnum::ROLE_ADMIN) {
                     $span .= '<span class="label label-primary" style="margin-right:10px">administrador</span>';
