2d9c6cbab6c1404d485297dadaecf3c25175a3a7
Wils Iglesias <wiglesias83@gmail.com>
2017-11-12 19:21:06 +0100

    improve base tcpdf

--- src/AppBundle/Pdf/BaseTcpdf.php
+++ src/AppBundle/Pdf/BaseTcpdf.php
@@ -15,10 +15,10 @@ use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 class BaseTcpdf extends \TCPDF
 {
     const PDF_WIDTH = 210;
-    const PDF_MARGIN_LEFT = 30;
-    const PDF_MARGIN_RIGHT = 30;
-    const PDF_MARGIN_TOP = 70;
-    const PDF_MARGIN_BOTTOM = 25;
+    const PDF_MARGIN_LEFT = 15;
+    const PDF_MARGIN_RIGHT = 15;
+    const PDF_MARGIN_TOP = 20;
+    const PDF_MARGIN_BOTTOM = 20;
     const MARGIN_VERTICAL_SMALL = 3;
     const MARGIN_VERTICAL_BIG = 8;
 
@@ -56,6 +56,16 @@ class BaseTcpdf extends \TCPDF
         $this->Image($this->ahs->getUrl('/bundles/app/img/logo-pdf.png'), 75, 20, 60);
         $this->SetXY(self::PDF_MARGIN_LEFT, 11);
         $this->setFontStyle(null, 'I', 8);
+    }
+
+    /**
+     * Page footer.
+     */
+    public function footer()
+    {
+        $this->SetXY(self::PDF_MARGIN_LEFT, 280);
+        $this->setFontStyle(null, 'I',  8);
+        $this->Cell(0,0, 'anything', 0, 0, 'T', 0, 'C');
     }
 
     /**
