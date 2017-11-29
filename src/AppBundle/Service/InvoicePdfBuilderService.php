6f5121d7a9524ccc65dcf2a1396c721626a34b0e
Wils Iglesias <wiglesias83@gmail.com>
2017-11-29 17:23:15 +0100

    improve invoice pdf

--- src/AppBundle/Service/InvoicePdfBuilderService.php
+++ src/AppBundle/Service/InvoicePdfBuilderService.php
@@ -3,16 +3,18 @@
 namespace AppBundle\Service;
 
 use AppBundle\Entity\Invoice;
+use AppBundle\Entity\InvoiceLine;
+use AppBundle\Entity\Setting;
 use AppBundle\Pdf\BaseTcpdf;
 use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 use WhiteOctober\TCPDFBundle\Controller\TCPDFController;
 use Symfony\Bundle\FrameworkBundle\Templating\Helper\AssetsHelper;
 
 /**
- * Class InvoicePdfBuilderService
+ * Class InvoicePdfBuilderService.
  *
  * @category Service
- * @package  AppBundle\Service
+ *
  * @author   Wils Iglesias <wiglesias83@gmail.com>
  */
 class InvoicePdfBuilderService
@@ -90,26 +92,41 @@ class InvoicePdfBuilderService
         $pdf->setFontStyle(null, '', 11);
         // contact
         $pdf->Write(0, 'FACTURA '.$invoice->getInvoiceNumber(), '', false, 'L', true);
-        $pdf->Write(0, $invoice->getDate()->format('d/m/Y'), '', false, 'L', true);
+        $pdf->Write(0, 'Fecha: '.$invoice->getDate()->format('d/m/Y'), '', false, 'L', true);
         $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_SMALL);
-        $pdf->Write(0,$invoice->getCustomer()->getName(), '', false, 'L', true);
+
+        $setting = new Setting();
+        $pdf->Write(0, $setting->getName(), '', false, 'L', true);
+
+        $pdf->Write(0, $invoice->getCustomer()->getName(), '', false, 'L', true);
         $pdf->Write(0, $invoice->getCustomer()->getIdentityCard(), '', false, 'L', true);
         $pdf->Write(0, $invoice->getCustomer()->getAddress(), '', false, 'L', true);
         $pdf->Write(0, $invoice->getCustomer()->getCity(), '', false, 'L', true);
         $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_SMALL);
-        // description
+        // table
         $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG);
-//        $pdf->setCellPaddings(2, 1, 0, 0);
-//        $pdf->setCellMargins(1, 0, 1, 0);
-
-        $pdf->MultiCell(100, 7, 'CONCEPTO', 0, 'L', false, 0, '', '', true, 0, true, true, 0, 'T', false);
-        $pdf->MultiCell(25, 7, 'CANTIDAD', 0, 'C', false, 0, '', '', true, 0, true, true, 0, 'T', false);
-        $pdf->MultiCell(25, 7, 'PRECIO', 0, 'C', false, 0, '', '', true, 0, true, true, 0, 'T', false);
-        $pdf->MultiCell(30, 7, 'TOTAL', 0, 'C', false, 0, '', '', true, 0, true, true, 0, 'T', false);
-
-
-//        $pdf->MultiCell(100, 7, 'CONCEPTO', 1, 'L', false, 1, '', '', true, 0, true, true, 0, 'T', false);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG);
+        $pdf->setCellPaddings(2, 1, 0, 0);
+        // table header
+//        $pdf->MultiCell(100, 7, 'CONCEPTO', 1, 'C', false, 0, '', '', true, 0, true, true, 0, 'T', false);
+//        $pdf->MultiCell(25, 7, 'CANTIDAD', 1, 'C', false, 0, '', '', true, 0, true, true, 0, 'T', false);
+//        $pdf->MultiCell(25, 7, 'PRECIO', 1, 'C', false, 0, '', '', true, 0, true, true, 0, 'T', false);
+//        $pdf->MultiCell(30, 7, 'TOTAL', 1, 'C', false, 0, '', '', true, 0, true, true, 0, 'T', false);
 //        $pdf->setCellMargins(1, 0, 0, 0);
+        // table body
+
+        /** @var InvoiceLine $line */
+        foreach ($invoice->getLines() as $line) {
+            $pdf->setFontStyle(null, '', 10);
+            $pdf->MultiCell(100, 7, $line->getName(), 0, 'L', false, 0, '', '', true, 0, true, true, 0, 'T', false);
+            $pdf->setCellMargins(0, 0, 0, 0);
+            $pdf->MultiCell(25, 7, $line->getAmount(), 0, 'R', false, 0, '', '', true, 0, true, true, 0, 'T', false);
+            $pdf->MultiCell(25, 7, number_format($line->getPrice(), 2, ',', '.').' €', 0, 'R', false, 0, '', '', true, 0, true, true, 0, 'T', false);
+            $pdf->MultiCell(30, 7, number_format($line->getTotal(), 2, ',', '.').' €', 0, 'R', false, 0, '', '', true, 0, true, true, 0, 'T', false);
+        }
+        $pdf->setCellMargins(1, 0, 1, 0);
+        $pdf->setCellPaddings(0, 0, 0, 0);
+        // table footer
 
         return $pdf;
     }
